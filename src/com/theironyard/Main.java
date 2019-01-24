4ed34bd55d55573b2848cb38c41c27edf7fa2396
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-24 11:38:43 -0500

    added try, finally block for extreme starvation

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,9 +1,11 @@
 package com.theironyard;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 import static java.lang.Thread.*;
 
 public class Main {
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
 
@@ -13,11 +15,11 @@ public class Main {
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 2");
 
-//        t1.setPriority(10);
-//        t2.setPriority(8);
-//        t3.setPriority(6);
-//        t4.setPriority(4);
-//        t5.setPriority(2);
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
 
         t3.start();
         t2.start();
@@ -39,9 +41,12 @@ public class Main {
         @Override
         public void run() {
             for(int i=0; i<100; i++) {
-                synchronized (lock) {
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
                     //execute critical section of code
+                }finally {
+                    lock.unlock();
                 }
             }
         }
